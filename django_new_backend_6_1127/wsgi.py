7b884aa09aa326ebbeb12da20819093f966210d2
Crowdbotics <team@crowdbotics.com>
2019-11-25 19:34:16 +0000

    Update models

--- django_new_backend_6_1127/wsgi.py
+++ django_new_backend_6_1127/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_new_backend_6_1127.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_new_backend_6_1127.settings")
 
 application = get_wsgi_application()
--- home/models.py
+++ home/models.py
@@ -6,27 +6,42 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150,)
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
     body = models.TextField()
+    r1 = models.BigIntegerField(blank=True, null=True,)
+    r2 = models.ForeignKey(
+        "home.CustomText",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="homepage_r2",
+    )
+    r3 = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="homepage_r3",
+    )
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
